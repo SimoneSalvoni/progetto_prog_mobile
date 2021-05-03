d719bcba3646ac5357867c27c9969500c03f8002
Simone Salvoni <simone.salvoni99@gmail.com>
2021-05-03 17:33:03 +0200

    aggiunto file words_3000.txt e classe ImpViewModel

---
+++ app/src/main/java/com/example/italian_englishgames/impiccato/ImpViewModel.kt
@@ -0,0 +1,71 @@
+package com.example.italian_englishgames.impiccato
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.italian_englishgames.R
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.random.Random
+
+class ImpViewModel: ViewModel() {
+
+    private val _chosenWord = MutableLiveData<String>("")
+    val chosenWord: LiveData<String>
+        get() = _chosenWord
+    private val _shownWord = MutableLiveData<String>()
+    val shownWord: LiveData<String>
+        get() = _shownWord
+    private val _errors = MutableLiveData<Int>(0)
+    val errors: LiveData<Int>
+        get() = _errors
+    private val chosenLetters = mutableSetOf<Char>()
+
+    enum class State {
+        PLAYING, WIN, LOSE
+    }
+
+    private val _gameState = MutableLiveData<State>(State.PLAYING)
+    val gameState: LiveData<State>
+    get() = _gameState
+
+    fun chooseWord(ctx: Context) {
+        /*
+        val bufferedReader: BufferedReader = File("R.raw.words_3000").bufferedReader()
+        */
+        val isr = InputStreamReader (ctx.resources.openRawResource(R.raw.words_3000))
+        val br = BufferedReader(isr)
+        val w: Int = Random.nextInt(1, 3000)
+        var s: String = ""
+        for (i in 1..w) s = br.readLine()
+        _chosenWord.value = s
+        for (i in 1..s.length) _shownWord.value += "-"
+    }
+
+    private fun letterIsPresent(letter: Char, pos: IntArray ) {
+        var arr = _shownWord.value!!.toCharArray()
+        for (i in 0..pos.size-1)  arr[pos[i]] = letter
+        _shownWord.value = String(arr)
+        chosenLetters.add(letter)
+        if (_shownWord==_chosenWord) _gameState.value=State.WIN
+    }
+
+    private fun letterIsNotPresent(letter:Char) {
+        chosenLetters.add(letter)
+        _errors.value = _errors.value?.plus(1)
+        if (errors.value==6) _gameState.value=State.LOSE
+    }
+
+    fun checkLetter(c: Char) {
+        val ind = mutableListOf<Int>()
+        for (i in 0.._chosenWord.value!!.length-1)
+            if (c==chosenWord.value!!.get(i)) ind.add(i)
+        if (ind.size==0) letterIsNotPresent(c)
+        else letterIsPresent(c, ind.toIntArray() )
+    }
+
+    fun ChosenLetterOk(c: Char): Boolean {
+        return !(c in chosenLetters)
+    }
+}
