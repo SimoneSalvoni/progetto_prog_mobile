3b2ade3cbda0f64d158a4861cb2cc894ea7d323b
Simone Salvoni <simone.salvoni99@gmail.com>
2021-05-05 12:35:38 +0200

    modificata la classe impViewModel

--- app/src/main/java/com/example/italian_englishgames/impiccato/ImpViewModel.kt
+++ app/src/main/java/com/example/italian_englishgames/impiccato/ImpViewModel.kt
@@ -1,16 +1,23 @@
 package com.example.italian_englishgames.impiccato
 
+import android.app.Application
 import android.content.Context
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.example.italian_englishgames.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import kotlin.random.Random
 
-class ImpViewModel: ViewModel() {
+class ImpViewModel(application: Application): AndroidViewModel(application) {
 
+    /* NON SO COME SI DOVREBBE PASSARE L'APPLICATION DALLA VIEW....*/
+
+    enum class State {
+        PLAYING, WIN, LOSE
+    }
     private val _chosenWord = MutableLiveData<String>("")
     val chosenWord: LiveData<String>
         get() = _chosenWord
@@ -20,27 +27,27 @@ class ImpViewModel: ViewModel() {
     private val _errors = MutableLiveData<Int>(0)
     val errors: LiveData<Int>
         get() = _errors
-    private val chosenLetters = mutableSetOf<Char>()
-
-    enum class State {
-        PLAYING, WIN, LOSE
-    }
-
     private val _gameState = MutableLiveData<State>(State.PLAYING)
     val gameState: LiveData<State>
-    get() = _gameState
+        get() = _gameState
+    private val chosenLetters = mutableSetOf<Char>()
+    private val ctx = getApplication<Application>().applicationContext
 
-    fun chooseWord(ctx: Context) {
-        /*
-        val bufferedReader: BufferedReader = File("R.raw.words_3000").bufferedReader()
-        */
+
+    suspend fun readFromFile(br: BufferedReader, times: Int ) {
+        var dest: String = ""
+        withContext(Dispatchers.IO) { for (i in 1..times) dest = br.readLine() }
+        _chosenWord.value = dest
+        for (i in 1..dest.length) _shownWord.value += "-"
+    }
+
+    fun chooseWord() {
         val isr = InputStreamReader (ctx.resources.openRawResource(R.raw.words_3000))
         val br = BufferedReader(isr)
-        val w: Int = Random.nextInt(1, 3000)
-        var s: String = ""
-        for (i in 1..w) s = br.readLine()
-        _chosenWord.value = s
-        for (i in 1..s.length) _shownWord.value += "-"
+        val word: Int = Random.nextInt(1, 3000)
+        viewModelScope.launch{
+            readFromFile(br,word)
+        }
     }
 
     private fun letterIsPresent(letter: Char, pos: IntArray ) {
@@ -65,7 +72,16 @@ class ImpViewModel: ViewModel() {
         else letterIsPresent(c, ind.toIntArray() )
     }
 
+    //per vedere se una lettere inserita è già stata usata
     fun ChosenLetterOk(c: Char): Boolean {
         return !(c in chosenLetters)
+    }
+
+    //non sono sicuro se è questo esattamente che dobbiamo fare nel reset
+    fun resetGame(){
+        _gameState.value=State.PLAYING
+        _errors.value=0
+        chosenLetters.clear()
+        chooseWord()
     }
 }
