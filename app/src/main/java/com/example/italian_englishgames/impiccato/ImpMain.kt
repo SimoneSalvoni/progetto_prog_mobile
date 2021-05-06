a6d21a606c02380729f0ea48dd74825ee2be1d37
Simone Salvoni <simone.salvoni99@gmail.com>
2021-05-06 17:41:14 +0200

    "commit intermedia"

--- app/src/main/java/com/example/italian_englishgames/impiccato/ImpMain.kt
+++ app/src/main/java/com/example/italian_englishgames/impiccato/ImpMain.kt
@@ -16,16 +16,5 @@ class ImpMain : AppCompatActivity() {
         setContentView(R.layout.activity_imp_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        private lateinit var GameModel: ImpViewModel
-
-        Log.i("GameFragment", "Called ViewModelProvider.get")
-        GameModel = ViewModelProvider(this).get(ImpViewModel::class.java)
-
-        findViewById<TextView>(R.id.displayedText).text = GameModel.shownword.get()
-        findViewById<TextView>(R.id.wrongChoice).text = GameModel.chosenLetters.get()
-        findViewById<Button>(R.id.guessButton).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
         }
     }
-}
