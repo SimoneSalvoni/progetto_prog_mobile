cc94de041982c20db7885bef1711d9573d469551
RGiova <66753387+RGiova@users.noreply.github.com>
2021-05-06 17:25:07 +0200

    EditView

--- app/src/main/java/com/example/italian_englishgames/impiccato/ImpGameFragment.kt
+++ app/src/main/java/com/example/italian_englishgames/impiccato/ImpGameFragment.kt
@@ -1,16 +1,10 @@
 package com.example.italian_englishgames.impiccato
 
-import android.content.Context
 import android.os.Bundle
-import android.os.VibrationAttributes
-import android.util.Log
-import android.view.LayoutInflater
-import android.widget.Button
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.lifecycle.ViewModel
 import com.example.italian_englishgames.R
 
 class ImpGameFragment : AppCompatActivity() {
@@ -22,30 +16,22 @@ class ImpGameFragment : AppCompatActivity() {
 
         lateinit var GameModel: ImpViewModel
 
+        val insertedText: String = findViewById<EditText>(R.id.guessText).getText().toString()
         /*Log.i("GameFragment", "Called ViewModelProvider.get")
         GameModel = ViewModelProvider(this).get(ImpViewModel::class.java)
 
         findViewById<TextView>(R.id.displayedText).text = GameModel.shownword.get()
         findViewById<TextView>(R.id.wrongChoice).text = GameModel.chosenLetters.get()*/
 
-        findViewById<Button>(R.id.guessButton).setOnClickListener { view ->
-            val builder = AlertDialog.Builder(this)
-            val inflater= LayoutInflater(Context.getSystemService)
-            val dialogLayout = inflater.inflate(R.layout.imp_guess_layout)
-            val guessText = dialogLayout.findViewById<TextView>(R.id.guessText)
-
-            with(builder) {
-                setTitle("Scrivi una lettera")
-                setPositiveButton("OK") {dialog, which ->
-                    findViewById<TextView>(R.id.displayedText).text = guessText.text.tostrng
+        findViewById<EditText>(R.id.guessText).setOnEditorActionListener { v, actionId, event ->
+            return@setOnEditorActionListener when (actionId) {
+                EditorInfo.IME_ACTION_SEND -> {
+                    findViewById<TextView>(R.id.wrongChoice).text = insertedText
+                    true
                 }
-                setNegativeButton("Cancella") {dialog, which ->
-                    Log.d("Main","Annullato")
-                }
-                setView(dialogLayout)
-                show()
-                }
+                else -> false
             }
         }
+
     }
 }
