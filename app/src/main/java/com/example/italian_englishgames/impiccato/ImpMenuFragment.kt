43075cf176cba6b0ea12e5feab85a5ce7707e7d4
Daniele Staffolani <danielestaffolani@libero.it>
2021-05-08 15:46:33 +0200

    modificate imp_menu_grament e ImpMenuFragment

---
+++ app/src/main/java/com/example/italian_englishgames/impiccato/ImpMenuFragment.kt
@@ -0,0 +1,27 @@
+package com.example.italian_englishgames
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+
+class ImpFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_impiccato, container, false)
+    }
+}
