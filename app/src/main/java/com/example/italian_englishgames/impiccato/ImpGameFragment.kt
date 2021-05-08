b15f8fb6ea8424f10183adc77feb883610263780
Simone Salvoni <simone.salvoni99@gmail.com>
2021-05-08 16:01:34 +0200

    aggiunto navgraph-imp e correzioni

--- app/src/main/java/com/example/italian_englishgames/impiccato/ImpGameFragment.kt
+++ app/src/main/java/com/example/italian_englishgames/impiccato/ImpGameFragment.kt
@@ -1,28 +1,37 @@
 package com.example.italian_englishgames.impiccato
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.italian_englishgames.R
 
-class ImpGameFragment : AppCompatActivity() {
+class ImpGameFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_imp_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_imp_game, container, false)
 
-        lateinit var GameModel: ImpViewModel
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+       // lateinit var GameModel: ImpViewModel
+    }
 
-        val insertedText: String = findViewById<EditText>(R.id.guessText).getText().toString()
         /*Log.i("GameFragment", "Called ViewModelProvider.get")
         GameModel = ViewModelProvider(this).get(ImpViewModel::class.java)
 
         findViewById<TextView>(R.id.displayedText).text = GameModel.shownword.get()
         findViewById<TextView>(R.id.wrongChoice).text = GameModel.chosenLetters.get()*/
-
+/*
         findViewById<EditText>(R.id.guessText).setOnEditorActionListener { v, actionId, event ->
             return@setOnEditorActionListener when (actionId) {
                 EditorInfo.IME_ACTION_SEND -> {
@@ -30,8 +39,7 @@ class ImpGameFragment : AppCompatActivity() {
                     true
                 }
                 else -> false
+*/
             }
-        }
 
-    }
-}
+
