0723dcdab6aff0ca8f67707512c98480431e2a20
RGiova <66753387+RGiova@users.noreply.github.com>
2021-05-11 21:07:37 +0200

    Game fragment
    Bisogna fare bene il binding

--- app/src/main/java/com/example/italian_englishgames/impiccato/ImpGameFragment.kt
+++ app/src/main/java/com/example/italian_englishgames/impiccato/ImpGameFragment.kt
@@ -1,45 +1,80 @@
 package com.example.italian_englishgames.impiccato
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.italian_englishgames.R
 
 class ImpGameFragment : Fragment() {
 
+    lateinit var binding: FragmentImpGameBinding
+    val viewModel: ImpViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_imp_game, container, false)
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+         binding = inflater.inflate(R.layout.fragment_imp_game, container, false)
+        return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-       // lateinit var GameModel: ImpViewModel
-    }
+        binding.gameViewModel = viewModel
 
-        /*Log.i("GameFragment", "Called ViewModelProvider.get")
-        GameModel = ViewModelProvider(this).get(ImpViewModel::class.java)
+        binding.chooseWord()
+        findViewById<ImageView>(R.id.impImageView).setImageResource(R.drawable.imp00)
+        findViewById<TextView>(R.id.displayedText).text = binding.shownword.get()
+        findViewById<TextView>(R.id.wrongChoice).text = binding.chosenLetters.get()
 
-        findViewById<TextView>(R.id.displayedText).text = GameModel.shownword.get()
-        findViewById<TextView>(R.id.wrongChoice).text = GameModel.chosenLetters.get()*/
-/*
         findViewById<EditText>(R.id.guessText).setOnEditorActionListener { v, actionId, event ->
             return@setOnEditorActionListener when (actionId) {
                 EditorInfo.IME_ACTION_SEND -> {
-                    findViewById<TextView>(R.id.wrongChoice).text = insertedText
+                    val car = binding.guessText.text.toString()
+                    binding.checkLetter(car)
+                    checkErrors()
+                    checkGameState()
                     true
                 }
                 else -> false
-*/
+
             }
+        }
 
+    }
 
+    fun checkErrors(){
+        var impiccato = findViewById<ImageView>(R.id.impImageView)
+        when(binding.errors){
+            1 -> impiccato.setImageResource(R.drawable.imp01)
+            2 -> impiccato.setImageResource(R.drawable.imp02)
+            3 -> impiccato.setImageResource(R.drawable.imp03)
+            4 -> impiccato.setImageResource(R.drawable.imp04)
+            5 -> impiccato.setImageResource(R.drawable.imp05)
+            6 -> impiccato.setImageResource(R.drawable.imp06)
+        }
+    }
+
+    fun checkGameState(){
+        when(binding.gameState){
+            "WIN" -> view?.findNavController()?.navigate(R.id.action_impMenuFragment_to_impWinFragment)
+            "LOSE" -> view?.findNavController()?.navigate(R.id.action_impMenuFragment_to_impLoseFragment)
+        }
+    }
+
+}
