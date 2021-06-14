5e1cffcb82acf98545496bd2ded36e14a5ed394a
Simone Salvoni <simone.salvoni99@gmail.com>
2021-06-14 18:29:25 +0200

    memViewModel e memWinFragment

--- app/src/main/java/com/example/italian_englishgames/memory/MemViewModel.kt
+++ app/src/main/java/com/example/italian_englishgames/memory/MemViewModel.kt
@@ -14,32 +14,32 @@ import java.io.InputStreamReader
 import kotlin.random.Random
 
 //VALORE PLACEHOLDER
-private const val NUM_OF_WORDS = 100
+private const val NUM_OF_WORDS = 286
 
 class MemViewModel(application: Application):AndroidViewModel(application) {
 
-    private val _words= MutableLiveData(mutableListOf<String>())
+    private val wordsAndIndices= mutableListOf<Pair<Int,String>>()
+    private val _words = MutableLiveData(mutableListOf<String>())
     val words: LiveData<MutableList<String>>
     get()=_words
-    var found_words= BooleanArray(36){false}
-    private val _errors = MutableLiveData<Int>(0)
-    val errors:LiveData<Int> //serve?
-        get() = _errors
+    var found_words= BooleanArray(16){false}
     private val ctx= getApplication<Application>().applicationContext
 
 
     fun init (){
         val indices = mutableSetOf<Int>()
-        for (i in 1..18) while(!indices.add(Random.nextInt(1, NUM_OF_WORDS))) //costanti da cambiare
+        for (i in 1..8) while(!indices.add(Random.nextInt(1, NUM_OF_WORDS))) //costanti da cambiare
             viewModelScope.launch{
                 var dest=""
                 var destList: List<String>
                 withContext(Dispatchers.IO) {
-                    for (i in 1..18){
+                    for (i in indices){
                         val isr = InputStreamReader (ctx.resources.openRawResource(R.raw.words_eng_it))
                         val br = BufferedReader(isr)
-                        for (j in 1..NUM_OF_WORDS)  dest=br.readLine()
+                        for (j in 1..i)  dest=br.readLine()
                         destList = dest.split(';')
+                        wordsAndIndices.add(Pair(i,destList[0]))
+                        wordsAndIndices.add(Pair(i,destList[1]))
                         _words.value?.add(destList[0])
                         _words.value?.add(destList[1])
                     }
@@ -49,13 +49,16 @@ class MemViewModel(application: Application):AndroidViewModel(application) {
     }
 
     fun check(word1: String, word2: String): Boolean{
-        if (word1!==word2){
-            _errors.value = _errors.value?.plus(1)
-            return false
+        var index1 = 0
+        var index2 = 0
+        for (elem in wordsAndIndices){
+            if (elem.second == word1) index1=elem.first
+            if (elem.second == word2) index2=elem.first
         }
+        if (index1!=index2) return false
         else {
-            found_words[words.value!!.indexOf(word1)]=true
-            found_words[words.value!!.indexOf(word2)]=true
+            found_words[_words.value!!.indexOf(word1)]=true
+            found_words[_words.value!!.indexOf(word2)]=true
             return true
         }
     }
---
+++ app/src/main/java/com/example/italian_englishgames/memory/MemWinFragment.kt
@@ -0,0 +1,36 @@
+package com.example.italian_englishgames.memory
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.findNavController
+import com.example.italian_englishgames.R
+
+
+class MemWinFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val inflater = inflater.inflate(R.layout.fragment_mem_win, container, false)
+        val retry = inflater.findViewById<Button>(R.id.retryButton)
+        retry.setOnClickListener{
+            retry.findNavController().navigate(R.id.action_memWinFragment_to_memGameFragment)
+        }
+        val home = inflater.findViewById<Button>(R.id.goHomeButton)
+        home.setOnClickListener{
+            home.findNavController().navigate(R.id.action_memWinFragment_to_memMenuFragment)
+        }
+        return inflater
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
