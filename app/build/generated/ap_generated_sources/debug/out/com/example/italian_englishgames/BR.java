3b1f0d78736e833a6be862c2930d4a9f5e2d82f3
RGiova <66753387+RGiova@users.noreply.github.com>
2021-06-29 21:37:24 +0200

    aggiornamento gitignore + rimozione build

--- app/build/generated/ap_generated_sources/debug/out/com/example/italian_englishgames/BR.java
+++
@@ -1,9 +0,0 @@
-package com.example.italian_englishgames;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int impViewModel = 1;
-
-  public static final int memViewModel = 2;
-}
--- app/build/generated/data_binding_trigger/debug/com/example/italian_englishgames/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.italian_englishgames;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
